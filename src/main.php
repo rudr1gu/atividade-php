dbd3f87fcea7bfd8747e8879809ce03f343d3a93
Rudr1gu <rodrigo.santos.ii@hotmail.com>
2024-03-14 22:33:01 -0300

    subindo arquivos

---
+++ src/main.php
@@ -0,0 +1,4 @@
+<?php echo("<h1>gol do são bernado</h1>");
+    include_once("./model/Carro.php");
+    $carro1 = new Carro();
+?> 
---
+++ src/model/Carro.php
@@ -0,0 +1,6 @@
+<?php
+class Carro{
+    function __construct(){
+        echo("Contrutor rodou...");
+    }
+}
